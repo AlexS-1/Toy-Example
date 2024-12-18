fdaab1e58ce911dfb59c6d4ad4bc8ff9000bf9b4
alexander.schranner <alexader.schranner@rwth-aachen.de>
2024-12-18 10:21:06 +0100

    Updated comments according to style guide and added new for-loop functionality

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     # NORMAL: Random number between 0 and 1
     variable = rand()
     
+    # NORMAL: Standard If-Else Block
     if True:
         print("This is True") # INLINE: Just for test purposes
     else:
@@ -15,10 +16,15 @@ def main():
         pass
 
     # BLOCK: If-Else Block
-    # BLOCK: to execute multiple lines of code
+    # BLOCK: To execute multiple lines of code
     if variable == True:
         print("Variable is True")
     else:
         print("Variable is False")
+
+    # BLOCK: For Loop
+    # BLOCK: To iterate over a sequence
+    for i in range(int(variable*10)):
+        print("Variable is bigger than 0,", i)
 
 main()
